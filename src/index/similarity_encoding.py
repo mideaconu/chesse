eaa5fab50760830a1256f81320dea33552147d20
mihai.deaconu <mihai.deaconu@benevolent.ai>
2022-01-02 14:21:38 +0000

    chore: add naive encoding

--- src/index/similarity_encoding.py
+++ src/index/similarity_encoding.py
@@ -1,9 +1,43 @@
 import chess
 
-def encode(fen: str):
+
+def get_naive_encoding(board: chess.Board) -> str:
+    """Returns the naive encoding of a given chess position.
+
+    The naive encoding represents the set of pieces availale on the given
+    board, in the format <PIECE><FILE><RANK>:
+        - <PIECE> is a letter corresponding to the piece type: q for queen,
+            k for king, b for bishon, n for knight, r for rook, and p for pawn.
+            To distinguish between colours, uppercase letters are being used
+            for white and lowercase letters for black.
+        - <FILE> is the file the piece is on, from a to h.
+        - <RANK> is the rank the piece is on, from 1 to 8.
+
+    Example: Rc8 is the white rook on c8, ph7 is the black pawn on h7.
+    """
+    naive_encoding = ""
+
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece:
+            naive_encoding += f"{piece}{chess.square_name(square)} "
+
+    return naive_encoding.strip()
+
+
+def get_similarity_encoding(fen: str):
+    similarity_encoding = ""
+
     board = chess.Board(fen=fen)
-    print(board)
+
+    similarity_encoding += get_naive_encoding(board)
+
+    return similarity_encoding
 
 
 if __name__ == "__main__":
-    encode("r1b2rk1/1pq1ppbp/p2p1np1/3N4/1P2P3/P2B4/2P1NPPP/1R1Q1RK1 b - -")
+    print(
+        get_similarity_encoding(
+            "r1b2rk1/1pq1ppbp/p2p1np1/3N4/1P2P3/P2B4/2P1NPPP/1R1Q1RK1 b - -"
+        )
+    )
