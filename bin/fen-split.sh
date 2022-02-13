e57cef24e5ab23bb2016df5bfd39a4aabca888ea
mihai.deaconu <mihai.deaconu@benevolent.ai>
2022-02-13 13:01:41 +0000

    chore: add fen-split script

---
+++ bin/fen-split.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -e
+
+print_usage() {
+   cat << EOF
+NAME
+    fen-split.sh -- splits a FEN file so that there is only one per position
+
+SYNOPSIS
+    fen-split.sh [-h/--help] [-o/--output-dir dir_name] file
+
+DESCRIPTION
+    Splits the source FEN file into one position per FEN file.
+
+    -h, --help
+        Print fen-split.sh documentation to stdout.
+
+    -o dir_name, --output-dir dir_name
+        The directory where the output is stored. By default, the output directory is the directory where the script is being called from.
+
+EXAMPLES
+    fen-split.sh file.fen
+        Splits file file.fen into one file per position into the current directory.
+    fen-split.sh -o data/split file.fen
+        Splits file file.fen into one file per position into the data/split directory.
+        
+EOF
+   exit 0
+}
+
+main() {
+    if [ $# -eq 0 ]; then print_usage; fi
+    for i in "$@" ; do [[ $i == "-h" ]] || [[ $i == "--help" ]] && print_usage ; done
+
+    while [[ $# -gt 1 ]]; do
+        key="$1"
+
+        case "$key" in 
+            -o|--output-dir)
+                OUTPUT_DIR="$2"
+                shift
+                shift
+                ;;
+            *)
+                echo "Unknown flag: $1"
+                exit 1
+                ;;
+        esac 
+    done
+
+    INPUT_FILE="${1}"
+
+    echo ${INPUT_FILE} ${INPUT_FILE%.*}
+    split -l 1 ${INPUT_FILE} ${INPUT_FILE%.*}
+    find . -type f ! -name "${INPUT_FILE%.*}*.*" -exec mv {} {}.fen \;
+    rm ${INPUT_FILE}
+
+
+    if ls ${INPUT_FILE%.*}*.fen 1>/dev/null 2>&1; then
+        if [[ ! -z "${OUTPUT_DIR}" ]]; then
+            mv ${INPUT_FILE%.*}*.fen ${OUTPUT_DIR}
+        fi
+    fi
+}
+
+main $@
