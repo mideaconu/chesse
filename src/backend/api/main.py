884eb14d6f43874606edf26a132986f079086f36
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-03-26 23:40:45 +0000

    chore: update server

--- src/backend/api/main.py
+++ src/backend/api/main.py
@@ -1,14 +1,12 @@
 from concurrent import futures
 
 import grpc
-from duchess_backend_api.v1alpha1 import duchess_pb2 as duchess_pb2_v1alpha1
-from duchess_backend_api.v1alpha1.duchess_pb2_grpc import (
-    add_DuchessBackendServiceServicer_to_server,
-)
+from chesse_backend_api.v1alpha1 import chesse_pb2 as chesse_pb2_v1alpha1
+from chesse_backend_api.v1alpha1.chesse_pb2_grpc import add_CheSSEBackendServiceServicer_to_server
 from grpc_reflection.v1alpha import reflection
 
 from backend.api import __version__
-from backend.api.v1alpha1.server import DuchessBackendService as DuchessBackendServiceV1Alpha1
+from backend.api.v1alpha1.server import CheSSEBackendService as CheSSEBackendServiceV1Alpha1
 from utils import logger
 
 LOGGER = logger.get_logger(__name__)
@@ -16,10 +14,10 @@ LOGGER = logger.get_logger(__name__)
 
 def serve(port: str, max_workers: int):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
-    add_DuchessBackendServiceServicer_to_server(DuchessBackendServiceV1Alpha1(), server)
+    add_CheSSEBackendServiceServicer_to_server(CheSSEBackendServiceV1Alpha1(), server)
 
     SERVICE_NAMES = (
-        duchess_pb2_v1alpha1.DESCRIPTOR.services_by_name["DuchessBackendService"].full_name,
+        chesse_pb2_v1alpha1.DESCRIPTOR.services_by_name["CheSSEBackendService"].full_name,
         reflection.SERVICE_NAME,
     )
 
@@ -36,8 +34,8 @@ if __name__ == "__main__":
     port = "50051"
     max_workers = 10
 
-    LOGGER.info("Starting Duchess Backend Service version %s", __version__)
-    LOGGER.info("Starting Duchess Backend Service on port: %s", port)
-    LOGGER.info("Starting Duchess Backend Service with max_workers: %s", max_workers)
+    LOGGER.info("Starting CheSSE Backend Service version %s", __version__)
+    LOGGER.info("Starting CheSSE Backend Service on port: %s", port)
+    LOGGER.info("Starting CheSSE Backend Service with max_workers: %s", max_workers)
 
     serve(port, max_workers)
--- src/backend/api/v1alpha1/server.py
+++ src/backend/api/v1alpha1/server.py
@@ -2,27 +2,27 @@ from ssl import create_default_context
 
 import chess
 import grpc
-from duchess_backend_api.v1alpha1 import games_pb2, positions_pb2
-from duchess_backend_api.v1alpha1.duchess_pb2_grpc import DuchessBackendServiceServicer
+from chesse_backend_api.v1alpha1 import chesse_pb2, games_pb2, positions_pb2
+from chesse_backend_api.v1alpha1.chesse_pb2_grpc import CheSSEBackendServiceServicer
 from elasticsearch import Elasticsearch
 
 from encoding.activity_encoding import encode as encode_activity
 from encoding.connectivity_encoding import encode as encode_connectivity
 from encoding.naive_encoding import encode as encode_naively
-from utils import logger
+from utils import logger, query
 
 LOGGER = logger.get_logger(__name__)
 
 
-class DuchessBackendService(DuchessBackendServiceServicer):
-    def GetSimilarGames(
-        self, request: games_pb2.GetSimilarGamesRequest, context: grpc.ServicerContext
-    ) -> games_pb2.GetSimilarGamesResponse:
-        """Retrieves similar chess games from DUChess."""
-        LOGGER.info("Retrieving games similar to position %s...", request.position_fen)
-        LOGGER.info("Get Similar Games Request: %s", request)
+class CheSSEBackendService(CheSSEBackendServiceServicer):
+    def GetSimilarPositions(
+        self, request: chesse_pb2.GetSimilarPositionsRequest, context: grpc.ServicerContext
+    ) -> chesse_pb2.GetSimilarPositionsResponse:
+        """Retrieves similar chess positions from CheSSE."""
+        LOGGER.info("Retrieving positions similar to %s...", request.position.fen)
+        LOGGER.info("Get Similar Positions Request: %s", request)
 
-        position_fen = request.position_fen
+        position_fen = request.position.fen
 
         board = chess.Board(fen=position_fen)
 
@@ -39,22 +39,90 @@ class DuchessBackendService(DuchessBackendServiceServicer):
         context = create_default_context(cafile="/Users/mihaideaconu/Documents/http_ca.crt")
         es = Elasticsearch(
             hosts=["https://localhost:9200"],
-            http_auth=("elastic", "lCUjVzlIk52a++cG6UCX"),
+            http_auth=("elastic", "QqLpzhLkR+Rm=b_dSaK9"),
             ssl_context=context,
         )
 
-        query = {"query": {"match": {"position.encoding": position_encoding}}}
+        positions_query = {"query": {"match": {"position.encoding": position_encoding}}}
 
-        res = es.search(index="positions", body=query)
+        res_pos = es.search(index="positions", body=positions_query)
 
-        response = games_pb2.GetSimilarGamesResponse(
-            games=[
-                games_pb2.Game(
-                    id=game["id"],
-                    position=positions_pb2.Position(fen=result["_source"]["position"]["fen"]),
+        fens = [position["_id"] for position in res_pos["hits"]["hits"]]
+        games_query = query.get_games_query(fens)
+        games_aggs = query.get_games_aggregation(fens)
+
+        final_games_query = {"query": games_query["query"], "aggs": games_aggs["aggs"]}
+
+        print(final_games_query)
+
+        res_gm = es.search(index="games", body=final_games_query)
+        # print([game["_id"] for game in res["hits"]["hits"]])
+
+        similar_positions = {}
+        for position in res_pos["hits"]["hits"]:
+            similar_positions[position["_id"]] = {"score": position["_score"]}
+
+        for position in res_gm["aggregations"]["by_fen"]["fen"]["buckets"]:
+            fen = position["key"].split(" ")[0]
+            similar_positions[fen]["nr_games"] = position["doc_count"]
+            similar_positions[fen]["rating_stats"] = {
+                "min": int(
+                    min(
+                        position["white"]["min_elo"]["value"], position["black"]["min_elo"]["value"]
+                    )
+                ),
+                "avg": (
+                    position["white"]["avg_elo"]["value"] + position["black"]["avg_elo"]["value"]
                 )
-                for result in res["hits"]["hits"]
-                for game in result["_source"]["games"]
+                / 2,
+                "max": int(
+                    max(
+                        position["white"]["max_elo"]["value"], position["black"]["max_elo"]["value"]
+                    )
+                ),
+            }
+
+            similar_positions[fen]["result_stats"] = {
+                "white_win_pct": 0,
+                "draw_pct": 0,
+                "black_win_pct": 0,
+            }
+            for side in position["results"]["white_won"]["buckets"]:
+                if side["key"] == 1:
+                    similar_positions[fen]["result_stats"]["white_win_pct"] = round(
+                        side["doc_count"] / similar_positions[fen]["nr_games"] * 100, 2
+                    )
+                elif side["key"] == 0.5:
+                    similar_positions[fen]["result_stats"]["draw_pct"] = round(
+                        side["doc_count"] / similar_positions[fen]["nr_games"] * 100, 2
+                    )
+                elif side["key"] == 0:
+                    similar_positions[fen]["result_stats"]["black_win_pct"] = round(
+                        side["doc_count"] / similar_positions[fen]["nr_games"] * 100, 2
+                    )
+
+        print(similar_positions)
+
+        response = chesse_pb2.GetSimilarPositionsResponse(
+            similar_positions=[
+                positions_pb2.SimilarPosition(
+                    position=positions_pb2.Position(fen=p_key),
+                    similarity_score=p_value["score"],
+                    position_stats=positions_pb2.PositionStats(
+                        nr_games=p_value["nr_games"],
+                        rating_stats=positions_pb2.PositionRatingStats(
+                            min=p_value["rating_stats"]["min"],
+                            avg=p_value["rating_stats"]["avg"],
+                            max=p_value["rating_stats"]["max"],
+                        ),
+                        result_stats=positions_pb2.PositionResultStats(
+                            white_win_pct=p_value["result_stats"]["white_win_pct"],
+                            draw_pct=p_value["result_stats"]["draw_pct"],
+                            black_win_pct=p_value["result_stats"]["black_win_pct"],
+                        ),
+                    ),
+                )
+                for p_key, p_value in similar_positions.items()
             ]
         )
 
