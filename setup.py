6ec465f6da78c3e4df4b9c114ba489bdf235457a
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-04-16 18:43:06 +0100

    chore: update requirements files

--- setup.py
+++ setup.py
@@ -3,17 +3,9 @@ from setuptools import setup
 setup(
     name="chesse",
     version="0.1.0",
-    py_modules=["bin.encode", "bin.pgn_to_json"],
-    install_requires=[
-        "chess==1.8.0",
-        "click==8.0.3",
-        "elasticsearch==8.1.0",
-        "google-api-python-client==2.39.0",
-        "grpcio==1.44.0",
-        "grpcio-tools==1.44.0",
-        "grpcio-reflection==1.44.0",
-    ],
+    py_modules=["scripts.encode", "scripts.pgn_to_json"],
+    install_requires=["chess==1.8.0", "click==8.0.3"],
     entry_points={
-        "console_scripts": ["encode = bin.encode:cli", "pgn-to-json = bin.pgn_to_json:cli"],
+        "console_scripts": ["encode = scripts.encode:cli", "pgn-to-json = scripts.pgn_to_json:cli"],
     },
 )
