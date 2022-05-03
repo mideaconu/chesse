35bc757036f78fc42d06be86e962e268176e771f
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-05-03 22:11:50 +0100

    chore: add get_games function to es_controller

--- src/backend/search_engine/controller/es_controller.py
+++ src/backend/search_engine/controller/es_controller.py
@@ -71,6 +71,95 @@ def _filter_position_stats(position_stats_raw: JSON) -> JSON:
     return positions_stats
 
 
+def _filter_games(games_raw: JSON) -> JSON:
+    """Filters the raw ES response so that only relevant information in the
+    games response is kept."""
+    games = {"games": {}, "stats": {}}
+    for game in games_raw["hits"]["hits"]:
+        games["games"][game["_id"]] = {
+            "context": {
+                "event": game["_source"]["context"]["event"],
+                "date": game["_source"]["context"]["date"],
+                "site": game["_source"]["context"]["site"],
+                "round": game["_source"]["context"]["round"],
+            },
+            "white": {
+                "name": game["_source"]["white"]["name"],
+                "elo": game["_source"]["white"]["elo"],
+            },
+            "black": {
+                "name": game["_source"]["black"]["name"],
+                "elo": game["_source"]["black"]["elo"],
+            },
+            "result": game["_source"]["result"],
+            "moves": game["_source"]["moves"],
+        }
+    print(games_raw["aggregations"])
+    games["stats"] = {
+        "nr_games": games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["doc_count"],
+        "rating": {
+            "min": int(
+                min(
+                    games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["white"][
+                        "min_elo"
+                    ]["value"],
+                    games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["black"][
+                        "min_elo"
+                    ]["value"],
+                )
+            ),
+            "avg": int(
+                (
+                    games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["white"][
+                        "avg_elo"
+                    ]["value"]
+                    + games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["black"][
+                        "avg_elo"
+                    ]["value"]
+                )
+                / 2
+            ),
+            "max": int(
+                max(
+                    games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["white"][
+                        "max_elo"
+                    ]["value"],
+                    games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["black"][
+                        "max_elo"
+                    ]["value"],
+                )
+            ),
+        },
+        "results": {
+            "white": 0,
+            "draw": 0,
+            "black": 0,
+        },
+    }
+    for side in games_raw["aggregations"]["category_fen"]["fen"]["buckets"][0]["results"][
+        "side_won"
+    ]["buckets"]:
+        match side["key"]:
+            case 1:
+                games["stats"]["results"]["white"] = (
+                    side["doc_count"] / games["stats"]["nr_games"] * 100
+                )
+            case 0.5:
+                games["stats"]["results"]["draw"] = (
+                    side["doc_count"] / games["stats"]["nr_games"] * 100
+                )
+            case 0:
+                games["stats"]["results"]["black"] = (
+                    side["doc_count"] / games["stats"]["nr_games"] * 100
+                )
+            case _:
+                logger.error(
+                    f"Error processing positions stats: illegal winning side: {side['key']}"
+                )
+
+    return games
+
+
 class ESController(controller_interface.AbstractSearchEngineController):
     def __init__(self) -> None:
         url = os.getenv("SEARCH_ENGINE_URL")
@@ -129,3 +218,30 @@ class ESController(controller_interface.AbstractSearchEngineController):
         logger.debug(positions_stats)
 
         return positions_stats
+
+    def get_games(self, fen: str) -> JSON:
+        fen_regex = f"{fen}.*"
+
+        query = es_dsl.Search(using=self.client, index="games").query(
+            "nested", path="moves", query=es_dsl.Q("regexp", moves__fen=fen_regex)
+        )
+        query.aggs.bucket("category_fen", "nested", path="moves").bucket(
+            "fen", "terms", field="moves.fen", include=fen_regex
+        )
+        query.aggs["category_fen"]["fen"].bucket("white", "reverse_nested").metric(
+            "min_elo", "min", field="white.elo"
+        ).metric("max_elo", "max", field="white.elo").metric("avg_elo", "avg", field="white.elo")
+        query.aggs["category_fen"]["fen"].bucket("black", "reverse_nested").metric(
+            "min_elo", "min", field="black.elo"
+        ).metric("max_elo", "max", field="black.elo").metric("avg_elo", "avg", field="black.elo")
+        query.aggs["category_fen"]["fen"].bucket("results", "reverse_nested").metric(
+            "side_won", "terms", field="result"
+        )
+
+        response = query.execute()
+        games_raw = response.to_dict()
+        games = _filter_games(games_raw)
+
+        logger.debug(games)
+
+        return games
