28e8e7eeb12e1ef0378306f9859c1e4dd79cc3c8
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-05-12 00:28:54 +0100

    chore: add test data

---
+++ tests/data/__init__.py
@@ -0,0 +1,23 @@
+import json
+
+from chesse_backend_api.v1alpha1 import positions_pb2
+
+with open("tests/data/chess_position.json") as chess_position_json:
+    chess_position_json = json.load(chess_position_json)
+
+chess_position_pb2 = positions_pb2.Position(
+    fen_encoding=chess_position_json["fen_encoding"],
+    position_stats=positions_pb2.PositionStats(
+        nr_games=chess_position_json["stats"]["nr_games"],
+        rating_stats=positions_pb2.PositionRatingStats(
+            min=chess_position_json["stats"]["rating"]["min"],
+            avg=chess_position_json["stats"]["rating"]["avg"],
+            max=chess_position_json["stats"]["rating"]["max"],
+        ),
+        result_stats=positions_pb2.PositionResultStats(
+            white_win_pct=chess_position_json["stats"]["results"]["white"],
+            draw_pct=chess_position_json["stats"]["results"]["draw"],
+            black_win_pct=chess_position_json["stats"]["results"]["black"],
+        ),
+    ),
+)
