ec4b002bd5f8679a0b638f199edc8e9e0e80f3c5
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-05-12 19:04:49 +0100

    test: add tests for GetChessGame and GetChessGames

--- tests/data/__init__.py
+++ tests/data/__init__.py
@@ -1,12 +1,18 @@
 import json
 
-from chesse_backend_api.v1alpha1 import positions_pb2
+from chesse_backend_api.v1alpha1 import games_pb2, positions_pb2
 
 with open("tests/data/chess_position.json") as chess_position:
     chess_position_json = json.load(chess_position)
 
 with open("tests/data/chess_positions.json") as chess_positions:
     chess_positions_json = json.load(chess_positions)
+
+with open("tests/data/chess_game.json") as chess_game:
+    chess_game_json = json.load(chess_game)
+
+with open("tests/data/chess_games.json") as chess_games:
+    chess_games_json = json.load(chess_games)
 
 chess_position_pb2 = positions_pb2.Position(
     fen_encoding=chess_position_json["fen_encoding"],
@@ -43,4 +49,57 @@ chess_positions_pb2 = [
         ),
     )
     for position in chess_positions_json
+]
+
+chess_game_pb2 = games_pb2.Game(
+    id=chess_game_json["id"],
+    context=games_pb2.GameContext(
+        event=chess_game_json["context"]["event"],
+        date=chess_game_json["context"]["date"],
+        site=chess_game_json["context"]["site"],
+        round=chess_game_json["context"]["round"],
+    ),
+    white=games_pb2.White(
+        name=chess_game_json["white"]["name"], elo=chess_game_json["white"]["elo"]
+    ),
+    black=games_pb2.Black(
+        name=chess_game_json["black"]["name"], elo=chess_game_json["black"]["elo"]
+    ),
+    result=chess_game_json["result"],
+    moves=[
+        games_pb2.Move(
+            uci=move["uci"],
+            san=move["san"],
+            fen=move["fen"],
+        )
+        for move in chess_game_json["moves"]
+    ],
+)
+
+chess_games_pb2 = [
+    games_pb2.Game(
+        id=chess_game_json["id"],
+        context=games_pb2.GameContext(
+            event=chess_game_json["context"]["event"],
+            date=chess_game_json["context"]["date"],
+            site=chess_game_json["context"]["site"],
+            round=chess_game_json["context"]["round"],
+        ),
+        white=games_pb2.White(
+            name=chess_game_json["white"]["name"], elo=chess_game_json["white"]["elo"]
+        ),
+        black=games_pb2.Black(
+            name=chess_game_json["black"]["name"], elo=chess_game_json["black"]["elo"]
+        ),
+        result=chess_game_json["result"],
+        moves=[
+            games_pb2.Move(
+                uci=move["uci"],
+                san=move["san"],
+                fen=move["fen"],
+            )
+            for move in chess_game_json["moves"]
+        ],
+    )
+    for chess_game_json in chess_games_json
 ]
--- tests/unit_tests/backend/api/v1alpha1/test_service.py
+++ tests/unit_tests/backend/api/v1alpha1/test_service.py
@@ -84,3 +84,45 @@ class TestCheSSEBackendService:
         assert all(
             position in test_data.chess_positions_pb2 for position in chess_positions.positions
         )
+
+    @mock.patch("backend.api.v1alpha1.service.pb2_utils.convert_json_to_pb2")
+    def test_GetChessGame(
+        self, mock_convert_json_to_pb2, mock_chesse_backend_server, mock_chesse_backend_client_stub
+    ):
+        # GIVEN
+        game_id = "eJwLSixJTVFwKs3JSS1RSE/MTa0xMjA01jMw1jMyqDE0tDIxtTI0qLGvSS9KTc2Oz0usyqzJz6hMTcwAABDeEwI="
+        MockCheSSEBackendController.get_chess_game.return_value = test_data.chess_game_json
+        mock_convert_json_to_pb2.return_value = test_data.chess_game_pb2
+
+        # WHEN
+        chess_game = mock_chesse_backend_client_stub.GetChessGame(
+            chesse_pb2.GetChessGameRequest(game_id=game_id)
+        )
+
+        # THEN
+        MockCheSSEBackendController.get_chess_game.assert_called_once_with(id=game_id)
+        mock_convert_json_to_pb2.assert_called_once_with(chess_game_json=test_data.chess_game_json)
+        assert chess_game.game == test_data.chess_game_pb2
+
+    @mock.patch("backend.api.v1alpha1.service.pb2_utils.convert_json_to_pb2")
+    def test_GetChessGames(
+        self, mock_convert_json_to_pb2, mock_chesse_backend_server, mock_chesse_backend_client_stub
+    ):
+        # GIVEN
+        fen_encoding = "3R2k1/pp3p2/2q3pp/8/1P2Q3/P3P1P1/5PKP/2r5"
+        MockCheSSEBackendController.get_chess_games.return_value = test_data.chess_games_json
+        mock_convert_json_to_pb2.return_value = test_data.chess_games_pb2
+
+        # WHEN
+        chess_games = mock_chesse_backend_client_stub.GetChessGames(
+            chesse_pb2.GetChessGamesRequest(fen_encoding=fen_encoding)
+        )
+
+        # THEN
+        MockCheSSEBackendController.get_chess_games.assert_called_once_with(
+            fen_encoding=fen_encoding
+        )
+        mock_convert_json_to_pb2.assert_called_once_with(
+            chess_games_json=test_data.chess_games_json
+        )
+        assert all(game in test_data.chess_games_pb2 for game in chess_games.games)
