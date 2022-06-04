96f700db7ba6fed57543d0e1d011da736e36e4c5
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-06-04 18:00:14 +0100

    chore: add setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="chesse",
-    version="0.1.0",
+    name="chesse-utils",
+    version="0.0.1",
     py_modules=["scripts.encode", "scripts.pgn_to_json"],
     install_requires=["chess==1.8.0", "click==8.0.3"],
     entry_points={
-        "console_scripts": ["encode = scripts.encode:cli", "pgn-to-json = scripts.pgn_to_json:cli"],
+        "console_scripts": [
+            "encode = scripts.encoding.encode:cli",
+            "pgn-to-json = scripts.processing.pgn_to_json:cli",
+        ],
     },
 )
