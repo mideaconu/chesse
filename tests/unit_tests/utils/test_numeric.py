616b2737c4a2162ffce1d80ac07ec892e76f2169
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-06-04 19:04:35 +0100

    test(encoding): add test to check if encoding is valid

--- tests/unit_tests/utils/test_numeric.py
+++ tests/unit_tests/utils/test_numeric.py
@@ -5,7 +5,7 @@ def test_is_int():
     assert numeric.is_int("3")
 
 
-def test_is_not_int():
+def test_is_int_fails():
     assert not numeric.is_int("three")
 
 
@@ -13,5 +13,5 @@ def test_is_float():
     assert numeric.is_float("3.2")
 
 
-def test_is_not_float():
+def test_is_float_fails():
     assert not numeric.is_float("three point two")
