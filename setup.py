1488e1441e78a17f75a41e407c66dafa8433d391
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-06-06 23:01:14 +0100

    chore: add package metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="chesse",
     version="0.0.1",
+    author="Mihai Ionut Deaconu",
+    author_email="mihai.ionut.deaconu@gmail.com",
+    description="CheSSE (Chess Similarity Search Engine) utility functions package",
+    long_description=long_description,
+    url="https://github.com/mideaconu/chesse",
     py_modules=["encoding.encode", "scripts.pgn_to_json"],
     install_requires=["chess==1.8.0", "click==8.0.3"],
     entry_points={
