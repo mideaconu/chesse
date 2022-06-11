d741df30481bd3dcc498c44fa8a539bf94b41250
Mihai Ionut Deaconu <mihaiionutdeaconu@gmail.com>
2022-06-11 17:48:42 +0100

    chore: update query tests

--- tests/integration_tests/backend/query/test_elasticsearch.py
+++ tests/integration_tests/backend/query/test_elasticsearch.py
@@ -17,57 +17,33 @@ def elasticsearch():
 
 def test_get_chess_position_query(elasticsearch):
     # GIVEN
-    chess_position_query = es_query.get_chess_position_query(
-        test_data.chess_position_json["fen_encoding"]
-    )
+    query = es_query.get_chess_position_query(test_data.chess_position_json["fen_encoding"])
 
     # THEN
-    assert elasticsearch.indices.validate_query(index="games", query=chess_position_query["query"])[
-        "valid"
-    ]
+    assert elasticsearch.indices.validate_query(index="games", query=query)["valid"]
 
 
 def test_get_similar_positions_query(elasticsearch):
     # GIVEN
-    similar_positions_query = es_query.get_similar_positions_query(
+    query = es_query.get_similar_positions_query(
         test_data.chess_position_json["similarity_encoding"]
     )
 
     # THEN
-    assert elasticsearch.indices.validate_query(
-        index="positions", query=similar_positions_query["query"]
-    )["valid"]
+    assert elasticsearch.indices.validate_query(index="positions", query=query)["valid"]
 
 
-def test_get_chess_positions_stats_query(elasticsearch):
+def test_get_chess_game_query(elasticsearch):
     # GIVEN
-    chess_positions_stats_query = es_query.get_chess_positions_stats_query(
-        [chess_position["fen_encoding"] for chess_position in test_data.chess_positions_json]
-    )
+    query = es_query.get_chess_game_query(test_data.chess_game_json["id"])
 
     # THEN
-    assert elasticsearch.indices.validate_query(
-        index="games", query=chess_positions_stats_query["query"]
-    )["valid"]
-
-
-def test_get_get_chess_game_query(elasticsearch):
-    # GIVEN
-    chess_game_query = es_query.get_chess_game_query(test_data.chess_game_json["id"])
-
-    # THEN
-    assert elasticsearch.indices.validate_query(index="games", query=chess_game_query["query"])[
-        "valid"
-    ]
+    assert elasticsearch.indices.validate_query(index="games", query=query)["valid"]
 
 
 def test_get_get_chess_games_query(elasticsearch):
     # GIVEN
-    chess_games_query = es_query.get_chess_games_query(
-        test_data.chess_position_json["fen_encoding"]
-    )
+    query = es_query.get_chess_games_query(test_data.chess_position_json["fen_encoding"])
 
     # THEN
-    assert elasticsearch.indices.validate_query(index="games", query=chess_games_query["query"])[
-        "valid"
-    ]
+    assert elasticsearch.indices.validate_query(index="games", query=query)["valid"]
